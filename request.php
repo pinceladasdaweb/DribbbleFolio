88093cf1ffc7557fd895311e6622b5e3063cfc09
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-12-04 15:38:10 -0200

    Call config class

--- request.php
+++ request.php
@@ -1,16 +1,22 @@
 <?php
 require_once './vendor/autoload.php';
 
-$classLoader = new SplClassLoader('DribbbleFolio', 'vendor');
-$classLoader->register();
+$helperLoader   = new SplClassLoader('Helpers', 'vendor');
+$dribbbleLoader = new SplClassLoader('DribbbleFolio', 'vendor');
 
+$helperLoader->register();
+$dribbbleLoader->register();
+
+use Helpers\Config;
 use DribbbleFolio\Dribbble;
+
+$config = new Config;
+$config->load('config/config.php');
 
 $user    = isset($_GET['username']) ? $_GET['username'] : null;
 $counter = isset($_GET['per_page']) ? $_GET['per_page'] : 18;
-$token   = '';
 
-$api   = new Dribbble($token);
+$api   = new Dribbble($config->get('dribbble.token'));
 $shots = $api->getUserShots($user, $counter);
 
 header('Content-type: application/json');
